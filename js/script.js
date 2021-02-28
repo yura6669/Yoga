e2a7d330f2731c8e2eb324b0ce54813ebae0dda3
Yura S <yura.shiyan01022000@gmail.com>
2021-02-28 20:02:53 +0200

    Slider

--- js/script.js
+++ js/script.js
@@ -41,7 +41,7 @@ window.addEventListener('DOMContentLoaded', function() {
     });
 
     // Створення таймеру зворотнього відліку
-    let deadline = '2021-02-21'; // Оголошення кінцевої дати
+    let deadline = '2021-03-01'; // Оголошення кінцевої дати
     
     // Функція, яка вираховує з мілісекунд секунди, хвилини та години
     let getTimeRemaining = (endtime) => {
@@ -260,5 +260,60 @@ window.addEventListener('DOMContentLoaded', function() {
     //     }
     // });
 
+    // Слайдер
+    let slideIndex = 1,
+        slides = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        dotsWrap = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot');
 
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        //slides.forEach((item) => item.style.display = 'none');
+        dots.forEach((item) => item.classList.remove('dot-active'));
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = 'none';
+        }
+
+        // for (let i = 0; i < dots.length; i++) {
+        //     dots[i].classList.remove('dot-active');
+        // }
+        slides[slideIndex -1].style.display = 'block';
+        dots[slideIndex -1].classList.add('dot-active');
+        
+    }
+
+    showSlides(slideIndex);
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    prev.addEventListener('click', function() {
+        plusSlides(-1);
+    });
+
+    next.addEventListener('click', function() {
+        plusSlides(1);
+    });
+
+    dotsWrap.addEventListener('click', function(event) {
+        for (let i = 0; i < dots.length +1; i++) {
+            if (event.target.classList.contains('dot') && event.target == dots[i - 1]) {
+                currentSlide(i);
+            }
+        }
+    });
 });
